7155cc1cd3fcc07d191ca499e56fbbed52975ee1
Kenji Yamashita <s16t278@stu.kagawa-u.ac.jp>
2019-02-06 21:00:04 +0900

    カレンダー描画成功

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,15 +1,4 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
-// vendor/assets/javascripts directory can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file. JavaScript code in this file should be added after the last require_* statement.
-//
-// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
-// about supported directives.
-//
+
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
@@ -21,5 +10,36 @@
 
 $(document).ready(function() {
     $('#calendar').fullCalendar({
+        header: {
+            left: 'prev, next, today',
+            center: 'title',
+            right: 'month, agendaWeek, agendaDay'
+        },
+        editable: true, 
+        selectable: true,
+        selectHelper: true, 
+        height: 600,
+        width: 400,
+
+        //ボタン文字列
+        buttonText: {
+            prev: '<',
+            next: '>',
+            today: '今日',
+            month: '月',
+            week: '週',
+            day: '日'
+
+        },
+        // 月名称
+        monthNames: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月'],
+        // 曜日略称
+        dayNamesShort: ['日', '月', '火', '水', '木', '金', '土'],
+        //月曜日開始
+        firstDay:1,
+        //agendaWeek, agendaDayの1時間4セル（15分間隔) で表示
+        //開始時間を朝7時に設定
+        scrollTime: '07:00:00',
+        defaultView: 'month',
     });
 });
