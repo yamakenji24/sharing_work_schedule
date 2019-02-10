8c9240f0a5954bbdef104dc5dfd3ce0fc192ccec
Kenji Yamashita <s16t278@stu.kagawa-u.ac.jp>
2019-02-10 15:37:26 +0900

    登録ができるようになった

--- app/controllers/event_controller.rb
+++ app/controllers/event_controller.rb
@@ -22,16 +22,15 @@ class EventController < ApplicationController
       event.attributes = {
         #user_id: @current_user.user_id,
         user_name: params[:title],
-        start_time: DataTime.parse(params[:start]),
-        end_time: DataTime.parse(params[:end])
+        start_time: params[:start],
+        end_time: params[:end]
       }
       event.save
       respond_to do |format|
         format.json {
           render json:
             event.to_json 
-            only: [:user_name, :start_time, :end_time]
-          
+            #only: [:user_name, :start_time, :end_time]          
         }  
       end
   end
